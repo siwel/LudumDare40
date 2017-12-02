ad75c8e6a6aa07e2475417a52a709ea0718d0778
Yeneneh Mulatu <yeneneh.mulatu@VFL-X-L-003.local>
2017-12-02 18:54:22 +0000

    adding pubsub

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
 import {Game} from './Game';
 
+import PubSub from 'pubsub-js'
+
 const game = new Game(document.getElementById('game'));
+
+// create a function to subscribe to topics
+var mySubscriber = function( msg, data ){
+    console.log( msg, data );
+};
+
+
+var token = PubSub.subscribe( 'MY TOPIC', mySubscriber );
+
+// publish a topic asyncronously
+PubSub.publish( 'MY TOPIC', 'hello world!' );
