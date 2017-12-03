e03d2b7487123f58dc950409e0fa01afa37bd4e8
Cerden <cmincher@hotmail.co.uk>
2017-12-03 13:54:34 +0000

    Changing background to block colour for now

--- src/states/Preloader.js
+++ src/states/Preloader.js
@@ -18,7 +18,7 @@ class Preloader extends Phaser.State {
 
     loadResources() {
 
-        this.game.load.image('background','image/desert_BG.png');
+        this.game.load.image('background','image/blueBg.png');
         //this.game.load.spritesheet('target', 'target.png',128.66,128);
         this.game.load.audio('bgSound','sound/bensound-betterdays.mp3');
 
