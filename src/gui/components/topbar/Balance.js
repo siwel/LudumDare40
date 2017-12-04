3d4a24cc932732aee4a9d6b87ec5b706232ac177
Lewis <lewis@lewisquaife.com>
2017-12-04 19:26:05 +0000

    Population & Balance gui

--- src/gui/components/topbar/Balance.js
+++ src/gui/components/topbar/Balance.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import coin from '../../../../assets/gui/icn_coin-x2.png'
+import shortNumber from 'short-number';
 import styles from '../../styles/gui.css';
 
 export class Balance extends React.Component {
 	render () {
 		const balance = this.props.value;
-		return <div className={styles.balance}>💰 {balance}</div>;
+		return <div className={styles.balance}><img className={styles.popImg} src={coin} /> {shortNumber(balance)}</div>;
 	}
 }
--- src/gui/components/topbar/Population.js
+++ src/gui/components/topbar/Population.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import co2btn from '../../../../assets/gui/icn_pp-x2.png'
+import shortNumber from 'short-number';
+
 
 import styles from '../../styles/gui.css';
 
 export class Population extends React.Component {
 	render () {
 		const population = this.props.value;
-		return <div className={styles.population}>👪 {population}</div>;
+		return <div className={styles.population}><img className={styles.popImg} src={co2btn} /><span>{shortNumber(population)}</span></div>;
 	}
 }
