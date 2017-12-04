a6147fe91994dd9eab6629e3fedf465dcd407b09
Lewis <lewis@lewisquaife.com>
2017-12-04 19:01:43 +0000

    co2 graphics

--- src/gui/components/topbar/CO2.js
+++ src/gui/components/topbar/CO2.js
@@ -1,15 +1,23 @@
 import React from 'react';
-
+import co2btn from '../../../../assets/gui/icn_co2-x2.png'
+import meterMark from '../../../../assets/gui/meter_mark-x2.png'
+import co2Bar from '../../../../assets/image/co2.png'
 import styles from '../../styles/gui.css';
 
 export class CO2 extends React.Component {
 	render () {
 		const co2 = this.props.value;
+
+		const width = (210/100) * co2;
 		return <div className={styles.co2}>
-			<span className={styles.co2__label}>CO<sub>2</sub></span>
-			<svg className={styles.co2__graph} viewBox="0 0 100 25">
-				<path d={`M0 0 h ${100 - co2} L0 25`} fill={"#0F0"} />
-			</svg>
+			<img className={styles.co2Icon} src={co2btn} />
+
+			<div className={styles.co2Bar} >
+				<div  className={styles.co2BarImageContainer} style={{width: width}}>
+					<img className={styles.co2BarImage} src={co2Bar} />
+				</div>
+				<img className={styles.co2BarMarker} src={meterMark} />
+			</div>
 		</div>;
 	}
 }
